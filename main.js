ead6f75b8dc30df73d36dfb81330ae514dba4d4d
jamesbonglee <66357217+jamesbonglee@users.noreply.github.com>
2020-07-30 23:44:26 +0900

    js-navbar menu toggle + project ul fixed(not complete)

--- main.js
+++ main.js
@@ -13,16 +13,23 @@ document.addEventListener("scroll", () => {
 
 // Handle scrolling when tapping on the navbar mene
 
-const navbarmenu = document.querySelector(".navbar__menu");
-navbarmenu.addEventListener("click", event => {
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", event => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toogle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
 //home__contact handle on [contact me] btn on home
 const homecontacme = document.querySelector(".home__contact");
 homecontacme.addEventListener("click", event => {
